184ee30c6d1566cfb321803201f99a458cb3a3ff
Biruk Bejga <birukbejga8@gmail.com>
2025-10-18 15:19:57 +0300

    feat: add popover component and integrate date-fns libraries
    - Introduced a new Popover component using Radix UI for better UI interactions.
    - Added date-fns and date-fns-tz libraries for improved date handling.
    - Updated schedule action to utilize new date handling functions and ensure proper scheduling logic.
    - Enhanced schedule validation by incorporating Google Calendar event checks and user availability.

--- constants/index.ts
+++ constants/index.ts
@@ -1,11 +1,12 @@
 export const DAYS_OF_WEEK_IN_ORDER = [
-    "Sunday",
+    
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
     "Saturday",
+    "Sunday",
 ] as const;
 export const PrivateNavLinks = [
     {
